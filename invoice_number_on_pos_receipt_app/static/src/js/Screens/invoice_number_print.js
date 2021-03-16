88e7003609074d5d6de86dc4c19c97dfc50e1bb9
Abraham Febres <afebres@xetechs.com>
2021-03-16 19:08:02 -0400

    refix: add fields in js

--- invoice_number_on_pos_receipt_app/static/src/js/Screens/invoice_number_print.js
+++ invoice_number_on_pos_receipt_app/static/src/js/Screens/invoice_number_print.js
@@ -63,6 +63,10 @@ odoo.define('invoice_number_on_pos_receipt_app.invoice_number_print', function (
 						var inv_print = output[0]['account_move'][1].split(" ")[0]
 						console.log( output  )
 						self.currentOrder.invoice_number = inv_print
+						self.currentOrder.fel_uuid = output[0]['fel_uuid']
+						self.currentOrder.fel_serie = output[0]['fel_serie']
+						self.currentOrder.fel_number = output[0]['fel_number']
+						self.currentOrder.fel_date = output[0]['fel_date']
 						self.showScreen(self.nextScreen);
 					})
 				}
@@ -93,74 +97,4 @@ odoo.define('invoice_number_on_pos_receipt_app.invoice_number_print', function (
 	Registries.Component.extend(PaymentScreen, PosInvPaymentScreen);
 
 	return PosInvPaymentScreen;
-
-
-
-	// screens.PaymentScreenWidget.include({
-	// 	finalize_validation: function() {
-	// 	var self = this;
-	// 	var order = this.pos.get_order();
-	// 	if (order.is_paid_with_cash() && this.pos.config.iface_cashdrawer) { 
-	// 			this.pos.proxy.printer.open_cashbox();
-	// 	}
-	// 	order.initialize_validation_date();
-	// 	order.finalized = true;
-	// 	if (order.is_to_invoice()) {
-	// 		var invoiced = this.pos.push_and_invoice_order(order);
-	// 		this.invoicing = true;
-	// 		invoiced.catch(this._handleFailedPushForInvoice.bind(this, order, false));
-	// 		invoiced.then(function (server_ids) {
-	// 			self.invoicing = false;
-	// 			var post_push_promise = [];
-	// 			post_push_promise = self.post_push_order_resolve(order, server_ids);
-	// 			post_push_promise.then(function () {
-	// 				rpc.query({
-	// 				model: 'pos.order',
-	// 				method: 'search_read',
-	// 				domain: [['pos_reference', '=', order['name']]],
-	// 				fields: ['account_move'],
-	// 				},{async:false})
-	// 				.then(function(output){
-	// 					var inv_print = output[0]['account_move'][1].split(" ")[0]
-	// 					order.invoice_number = inv_print;
-	// 					self.gui.show_screen('receipt');
-	// 				});
-	// 			}).catch(function (error) {
-	// 				self.gui.show_screen('receipt');
-	// 				if (error) {
-	// 					self.gui.show_popup('error',{
-	// 						'title': "Error: no internet connection",
-	// 						'body': error,
-	// 					});
-	// 				}
-	// 			});
-	// 		});
-	// 	} else {
-	// 		var ordered = this.pos.push_order(order);
-	// 		if(order.wait_for_push_order()){
-	// 			var server_ids = [];
-	// 			ordered.then(function (ids) {
-	// 			server_ids = ids;
-	// 			}).finally(function() {
-	// 				var post_push_promise = [];
-	// 				post_push_promise = self.post_push_order_resolve(order, server_ids);
-	// 				post_push_promise.then(function () {
-	// 				self.gui.show_screen('receipt');
-	// 				}).catch(function (error) {
-	// 					self.gui.show_screen('receipt');
-	// 					if (error) {
-	// 						self.gui.show_popup('error',{
-	// 							'title': "Error: no internet connection",
-	// 							'body':  error,
-	// 						});
-	// 					}
-	// 				});
-	// 			});
-	// 		}
-	// 		else {
-	// 		  self.gui.show_screen('receipt');
-	// 		}
-	// 	}
-	// },
-	// });
 });
